87a7b4c2281bad8e31dbf4d4f8cb6ed03fa25e05
Bagas N <nuriksabagas@gmail.com>
2023-07-05 02:48:16 +0700

    feat: update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -2,5 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+if test -f /usr/share/autojump/autojump.fish
+  source /usr/share/autojump/autojump.fish
+end
+
 set -gxa PATH "$HOME/.local/bin:"
 set -gx EDITOR nvim
